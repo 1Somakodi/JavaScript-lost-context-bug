e07530fcba430b01f4314d61d2a509ca855df725
Allan Ochieng <info.somakodi@gmail.com>
2024-10-01 07:39:33 +0300

    Initial files upload
    New files upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const messageConfig = {
+  frontContent: "Happy Birthday, Odin One-Eye!",
+  insideContent: "From Asgard to Nifelheim, you're the best all-father ever.\n\nLove,",
+  closing: {
+      "Thor": "Admiration, respect, and love",
+      "Loki": "Your son"
+  },
+  signatories: [
+      "Thor",
+      "Loki"
+  ]
+};
